079bfd596f8582dcafad9c1008143c79241f8d1f
Irupe <irupecanet@gmail.com>
2020-04-02 20:41:59 +0100

    Second commit

--- collatz.py
+++ collatz.py
@@ -12,11 +12,12 @@ n = int (input("Please enter a positive integer:"))
 while n != 1:
     if n % 2 == 0:
         n = int (n / 2)
-        print(n)
+        print(n, end=" ")
     
     else:
         n = int ((n * 3) + 1)
-        print(n)
+        print(n, end=" ")
+
         
 
     
