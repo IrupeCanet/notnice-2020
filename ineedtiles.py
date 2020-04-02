a0d671ace8c129ed6d1c98eaa21d673f160593e1
Irupe <irupecanet@gmail.com>
2020-04-02 09:08:59 +0100

    Added new files

---
+++ ineedtiles.py
@@ -0,0 +1,27 @@
+<<<<<<< HEAD
+# This program calculates how many tiles you will need
+# when tilling a wall or floor (in m2).
+
+length = 3.5
+width = 2.3
+
+area = length * width
+
+needed = area * 1.05
+
+print("You need", needed, "tiles in metres squared")
+
+=======
+# This program calculates how many tiles you will need
+# when tilling a wall or floor (in m2).
+
+length = 3.5
+width = 2.3
+
+area = length * width
+
+needed = area * 1.05
+
+print("You need", needed, "tiles in metres squared")
+
+>>>>>>> b5ff70a897922ec55d49c38e72b8e0b165458383
---
+++ secondstring.py
@@ -0,0 +1,8 @@
+# Irupe Canet
+# Variables
+# Weekly task 3: Python code to reverse string
+
+
+string = "The quick brown fox jump over the lazy dog."
+
+print(string[0:43:2])
