59576bbbc1d64648a296e59a2d91699fe6a0baf0
Irupe <irupecanet@gmail.com>
2020-04-03 10:51:15 +0100

    Third Commit

---
+++ squareroot.py
@@ -0,0 +1,13 @@
+#Irupe Canet
+# Weekly task number 6.
+# Write a program that takes a positve floating 
+# number as imput and outputs an aproximation of its square root.
+
+import numpy as np
+import math as mt
+import cmath as cm
+import sys
+
+#get number from user
+
+if number < 0
