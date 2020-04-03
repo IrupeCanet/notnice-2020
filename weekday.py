64c6d403524818b110a93850224304e2bf7e7c3c
Irupe <irupecanet@gmail.com>
2020-04-03 10:24:42 +0100

    Second Commmit

---
+++ weekday.py
@@ -0,0 +1,16 @@
+# Irupe Canet
+# Weekly task number 5
+# Program that outputs whether or not today is a weekday.
+
+import datetime
+day = datetime.datetime.now()
+
+string = day.strftime("Monday, Tuesday, Wednesday, Thursday, Fridat")
+
+
+if string [0] == string(Monday, Tuesday, Wednesday, Thursday, Friday):
+    print("Yes, unfortunatly today is a weekday")
+else:
+    print("It is the weekend, yay!")
+
+
