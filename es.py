2c78baf067c5f81580066578d913eb3629d8a428
Irupe <irupecanet@gmail.com>
2020-04-03 16:13:39 +0100

    Third Commit

--- es.py
+++ es.py
@@ -1,16 +1,12 @@
-# Irupe Canet
+#Irupe Canet
 # Weekly task number 7
-# Write a program that reads in a text file
+# Write a program that reads the file "moby-dick.txt"
 # and outputs the number of e's it contains.
 
 with open ('mobydick.txt', 'r') as f:
-    for line in f:
-        words = line.split()
-        for i in words:
-            for letter in i:
-                if(letter == e):
-                    e = e + 1
-print(e)                    
+    string = f.read()
+    count = string.count("e")
+print(count)
 
 
 
--- prove.py
+++ prove.py
@@ -1,6 +1,7 @@
-
-import collections
-import pprint
+#Irupe Canet
+# Weekly task number 7
+# Write a program that reads the file "moby-dick.txt"
+# and outputs the number of e's it contains.
 
 with open ('mobydick.txt', 'r') as f:
     string = f.read()
