e1d987589e0ce6e8683466adf33fa82c19e379b5
Irupe <irupecanet@gmail.com>
2020-04-03 13:21:45 +0100

    Third commit

--- es.py
+++ es.py
@@ -3,8 +3,17 @@
 # Write a program that reads in a text file
 # and outputs the number of e's it contains.
 
-with open ('mobydick', 'r') as f:
-    print(f.readline())
+e = 0
+
+with open ('mobydick.txt', 'r') as f:
+    for line in f:
+        words = line.split()
+        for i in words:
+            for letter in i:
+                if(letter==l):
+                    e=e=1
+print("Occurrences of the letter:")
+print(e)                    
 
 
 
